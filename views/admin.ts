12d07e991fe8986482d2e9ba6ca32c4fbbd14f1f
Prabhat Ranjan <Prabhat0206@hotmail.com>
2022-04-07 13:29:59 +0530

    change_condition

--- views/admin.ts
+++ views/admin.ts
@@ -196,9 +196,7 @@ admin
 admin.post("/change_condition", checkAdmin, async (req, res) => {
   const condition = req.query;
   const key = Object.keys(condition)[1];
-  const value = Number(condition[key[1]]) === 0 ? false : true;
-  console.log(condition)
-  console.log(key, value)
+  const value = Number(condition[key]) === 0 ? false : true;
   await Product.findOneAndUpdate({ _id: condition._id }, { key: value })
   return res.json({
     Success: true,
