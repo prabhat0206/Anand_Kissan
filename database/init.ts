ae1a0adeef71e9e5e899df98fab46a9b84041f8e
Prabhat Ranjan <Prabhat0206@hotmail.com>
2022-04-13 09:49:07 +0530

    migration

--- database/init.ts
+++ database/init.ts
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 import {config} from "dotenv"
 
 config()
-mongoose.connect(process.env.MONGO_DB_URI);
+mongoose.connect(process.env.MONGO_DB_URI || "");
 
 export default mongoose;
